f1f9f421c1514b1814d2de800758d5fe5fe3d63c
gregtsimmons <gregtsimmons@gmail.com>
2024-04-16 16:30:23 +0900

    user accounts, and static files

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog", #new
+    "blog", 
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,15 @@ STATICFILES_DIRS = [BASE_DIR/"static"]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # new
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
